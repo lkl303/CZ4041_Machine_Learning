2ec53fca62f9a5399adcd76d361a03dac76846ad
ddanielfung <70476971+ddanielfung@users.noreply.github.com>
2023-10-01 14:15:28 +0800

    added v1 of binary classification
    Classifier AUC: 0.914 (1st place)
    ours: 0.89
    no outlier Regression CV:1.545  (1st place)
    ours: 1.552

---
+++ imports.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import lightgbm as lgb
+from sklearn.model_selection import KFold
+import warnings
+import time
+import sys
+import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import mean_squared_error
+
+print("All dependencies present")
